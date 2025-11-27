02d1212cf66d85b13f7e552deabdf35253236ecf
Aidan <aidan.nickels07@gmail.com>
2025-11-27 23:13:30 +0100

    Power Modus zum PowerDot hinzugefügt. Geister fehlen aber sollte im Prinzip klappen

--- PowerDot.java
+++ PowerDot.java
@@ -1,18 +1,22 @@
-public class Power_Dots extends Figur
+public class PowerDot extends Figur
 {
-    public Power_Dots()
+    public PowerDot()
     {
         super();
-        
+       
         FigurteilFestlegenEllipse(-20, -20, 40, 40, "Orange");
+       
     }
 
     @Override
     void AktionAusführen()
     {
-       
+        
         if (Berührt("Gelb"))
         {
+           
+            PowerModeManager.aktivierePowerMode();
+
             Entfernen();
         }
     }
