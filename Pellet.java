5b1acb714cbdac289aceb9d77bd9fea5c2b3f228
wandeltn <niklas.wandelt@gmail.com>
2025-12-03 22:44:14 +0100

    Almost fix pellet positioning

--- Pellet.java
+++ Pellet.java
@@ -3,9 +3,9 @@ public class Pellet extends Figur {
     public Pellet(int tileCenterX, int tileCenterY) {
         // PositionSetzen erwartet die figuren-Mittelpunkt-Koordinate
         // tileCenterX/tileCenterY sollten bereits col*10 + OFFSET_X + 5 sein
-        PositionSetzen(tileCenterX, tileCenterY);
+        PositionSetzen(0, 0);
         // kleines Quadrat 6x6 zentriert (linke obere Ecke rel. zum Mittelpunkt: -3,-3)
-        FigurteilFestlegenRechteck(-3, -3, 6, 6, "gelb");
+        FigurteilFestlegenRechteck(tileCenterX, tileCenterY, 6, 6, "gelb");
         NachVornBringen(); // sicherstellen, dass Pellets sichtbar vor Wänden liegen
     }
     // keine Aktion/Entfernen überschreiben -> Figur.Entfernen() wird benutzt
--- PelletManager.java
+++ PelletManager.java
@@ -29,8 +29,8 @@ public class PelletManager {
             for (int col = 0; col < line.size(); col++) {
                 int tile = line.get(col);
                 // tile top-left coords:
-                int tileY = col * 10 + offsetX;
-                int tileX = row * 10 + offsetY;
+                int tileY = col * 10 + offsetY;
+                int tileX = row * 10 + offsetX;
                 // center coords for our Figuren (figuren zeichnen relativ zum Mittelpunkt)
                 int centerY = tileX + 5;
                 int centerX = tileY + 5;
@@ -38,22 +38,25 @@ public class PelletManager {
                 Logger.log("Checking pos for pellets at: x=" + row + " y=" + col + " val=" + tile, LogLevel.ERROR);
 
                 if (tile == 3) { // Pellet
+                    Logger.log("Spawning new Pellet at: x=" + centerX + " y=" + centerY, LogLevel.ERROR);
                     Pellet p = new Pellet(centerX, centerY);
                     pelletListe.add(p);
-                    pelletCount++;
+                    
                 } else if (tile == 4) { // PowerDot
+                    Logger.log("Spawning new PowerDot at: x=" + centerX + " y=" + centerY, LogLevel.ERROR);
                     PowerDot pd = new PowerDot(centerX, centerY);
                     pelletListe.add(pd);
-                    pelletCount++;
                 }
             }
         }
 
-        System.out.println("PelletManager: Pellets + PowerDots gespawnt = " + pelletCount);
         // bring all to front (nochmal, sicher)
         for (Figur f : pelletListe) {
             f.NachVornBringen();
+            pelletCount++;
         }
+        
+        System.out.println("PelletManager: Pellets + PowerDots gespawnt = " + pelletCount);
     }
 
     public static void removePellet(Figur f) {
@@ -63,7 +66,7 @@ public class PelletManager {
         }
     }
     
-    public static void registerPellet(Pellet pellet)
+    public static void registerPellet(Figur pellet)
     {
         pelletListe.add(pellet);
     }
